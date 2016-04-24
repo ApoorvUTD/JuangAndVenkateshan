c38665568d905b210ee29380fae914580a512252
Apoorv Kumar <axk132231@utdallas.edu>
2016-04-24 00:58:26 -0500

    implemented vector clock
    Change-Id: Ie5471f3aad87f7736107d321d5bbe22d43828492

--- JuangAndVenkateshan/src/Clock.java
+++ JuangAndVenkateshan/src/Clock.java
@@ -1,21 +1,66 @@
+import java.util.Arrays;
 
 public class Clock {
-	  public static Integer value = 0;
+      public static Integer value = 0;
+      public static int vectorClock[]=new int[ConfigReader.getNumberOfNodes()];
+      
+      //increment the clock
+ 
       public  static void incrClock(){
-   	      value++;
-   	   
-     }
-     public static int getValue(){
-   	  return value;
-     }
-	 public static void updateClock(int value) {
-		  if(Clock.value < value){
-			  Clock.value = value + 1;
+    	  synchronized(vectorClock){
+    	      vectorClock[ConfigReader.getMe()]++;
+      }
+      }
+      
+      
+      public static int getValue(){
+    	  return vectorClock[ConfigReader.getMe()];
+      }
+      
+      //update the clock
+	  public  static void updateVectorClock(String vectorClockReceived[])
+      {
+		  synchronized(vectorClock){
+    	  for(int i=0;i<ConfigReader.getNumberOfNodes();i++)
+    	  {
+    		  if(i==ConfigReader.getMe())
+    		  vectorClock[i]=maximum(vectorClock[i],Integer.parseInt(vectorClockReceived[i]))+1;
+    		  else
+    		  vectorClock[i]=maximum(vectorClock[i], Integer.parseInt(vectorClockReceived[i]));
+    	  }
 		  }
-		  else{
-			  Clock.value = Clock.value + 1;
-		  }
-		 
-	}
-
+      }
+	  
+	  
+      public  static String getVectorClock()
+      {
+    	  synchronized(vectorClock){
+    	  String vectorClockAppend=new String();
+    	  for(int i=0;i<ConfigReader.getNumberOfNodes();i++)
+    	  {
+    		if(i!=ConfigReader.getNumberOfNodes()-1)  
+    	  vectorClockAppend+=(vectorClock[i]+"~");
+    		else
+    	  vectorClockAppend+=(vectorClock[i]);
+    	  }
+    	  return vectorClockAppend;
+    	  }
+      }
+      public static  int maximum(int a, int b)
+      {
+    	  if(a<b)
+    		  return b;
+    	  else
+    		  return a;
+      }
+      
+     
+      public static String[] readVector(String tokens []){
+    	   return  Arrays.copyOfRange(tokens,2,tokens.length);
+      }
+      public static int returnClockValue(String tokens[],int index)
+      {
+    	  String [] clockArray=Clock.readVector(tokens);
+    	  return Integer.parseInt(clockArray[index]);
+      }
 }
