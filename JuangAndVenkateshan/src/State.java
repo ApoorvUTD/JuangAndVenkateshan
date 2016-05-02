a1a8b01bde2ebff8b13fab34ac2a1bc2d354bf5a
Sarat Chandra <sarat.vrns@gmail.com>
2016-05-02 07:57:47 -0500

    fixing compile error on State.java
    Change-Id: I2ec79e48ece3096218decda1d9604c3dfa8d4f57

--- JuangAndVenkateshan/src/State.java
+++ JuangAndVenkateshan/src/State.java
@@ -17,29 +17,9 @@ public class State {
 	}
 	
 
-	public State(boolean active, int maxNumber, int[] clock, int[] sentMsgs) {
-		super();
-		this.active = active;
-		this.maxNumber = maxNumber;
-		this.clock = clock;
-		this.sentMsgs = sentMsgs;
-	}
-	
-	
-
-
-	public State( int[] recievedMsgs,boolean active, int maxNumber, int[] clock) {
-		super();
-		this.active = active;
-		this.maxNumber = maxNumber;
-		this.clock = clock;
-		this.recievedMsgs = recievedMsgs;
-	}
-
-
 	@Override
 	public String toString() {
-		return "State [active=" + active + ", maxNumber=" + maxNumber + ", clock=" + Arrays.toString(clock)
+		return "State [active=" + active + ", sentCount=" + sentCount + ", clock=" + Arrays.toString(clock)
 				+ ", recievedMsgs=" + Arrays.toString(recievedMsgs) + ", sentMsgs=" + Arrays.toString(sentMsgs) + "]";
 	}
 	
